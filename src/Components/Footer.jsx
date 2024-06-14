fb1fb4ebdba43a28cb5af4eee04fe688fa2fa7e2
pavan kumar <pavan9346n>
2024-06-14 16:18:59 +0530

    Signed-off-by: pavan kumar <pavan9346n>

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,6 @@
+function Footer(){
+    return(
+        <h1>this is the footer</h1>
+    )
+}
+export default Footer;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header(){
+    return(
+        <div>
+        <h1>hellow</h1>
+        <h2>hii</h2>
+        </div>
+    )
+}
+export default Header;
